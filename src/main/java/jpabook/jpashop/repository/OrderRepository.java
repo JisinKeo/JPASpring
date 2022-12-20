1db050fa438e897f190c2d6c31638cb941f3283b
JisinKeo <tntjd9514@naver.com>
2022-12-20 18:31:02 +0900

    주문 서비스 개발

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -20,7 +20,7 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-//    public List<Order> findAll(OrderSearch){
+//    public List<Order> findAll(OrderSearch orderSearch){
 //
 //    }
 }
